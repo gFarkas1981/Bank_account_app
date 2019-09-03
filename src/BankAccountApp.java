58b2a8a36f43a2a1bd88d4dfb4a6d4d429f5da89
gFarkas1981 <info@gfarkas.com>
2019-09-03 15:41:01 +0200

    Enhancing application with correct access specifiers

--- src/BankAccountApp.java
+++ src/BankAccountApp.java
@@ -1,24 +1,38 @@
+import java.util.List;
+
 public class BankAccountApp {
 
     public static void main(String[] args) {
 
-        Checking chkacc1 = new Checking("John Doe", "453535365", 1500);
+        String file = "NewBankAccounts.csv";
 
-        Savings savacc1 = new Savings("Rich Lowe", "467384658", 2500);
-
-        chkacc1.showInfo();
-
-        System.out.println("************************");
-
-        savacc1.showInfo();
-
-        System.out.println("************************");
-        System.out.println("************************");
-
-        savacc1.compound();
+//        Checking chkacc1 = new Checking("John Doe", "453535365", 1500);
+//
+//        Savings savacc1 = new Savings("Rich Lowe", "467384658", 2500);
+//
+//        chkacc1.showInfo();
+//
+//        System.out.println("************************");
+//
+//        savacc1.showInfo();
+//
+//        System.out.println("************************");
+//        System.out.println("************************");
+//
+//        savacc1.compound();
 
 
         // Read a CSV File then create new accounts based on that data
+        List<String[]> newAccountHolders = CSV.read(file);
+
+        for (String[] accountHolder : newAccountHolders) {
+
+            System.out.println(accountHolder[0]);
+            System.out.println(accountHolder[1]);
+            System.out.println(accountHolder[2]);
+            System.out.println(accountHolder[3]);
+
+        }
 
     }
 
